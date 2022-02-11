c69906bfac528d72534eaa82bc899e79f37e318e
Sakib Ahmed <sakib.ahmed@onepeloton.com>
2022-02-10 20:35:43 -0500

    format

--- src/App.js
+++ src/App.js
@@ -1,41 +1,22 @@
-import './App.css';
-import React from 'react';
-import WorkoutList from './Components/WorkoutList';
-import Login from './Components/Login';
-import {
-  BrowserRouter as Router,
-  Routes,
-  Route,
-  Link
-} from "react-router-dom";
-
+import "./App.css";
+import React from "react";
+import WorkoutList from "./Components/WorkoutList";
+import Login from "./Components/Login";
+import { BrowserRouter as Router, Routes, Route, Link } from "react-router-dom";
 
 function App() {
   return (
-    <Router> 
-    
+    <Router>
       <nav>
-        <Link to = "/workoutlist"> WorkoutList </Link>
-        <Link to = "/login"> Login </Link>
+        <Link to="/workoutlist"> WorkoutList </Link>
+        <Link to="/login"> Login </Link>
       </nav>
-
-   
       <Routes>
-        
-        <Route path ="/workoutlist" element= {<WorkoutList />} />
-        
-        <Route path ="/login" element = {<Login />} />
-        
-
+        <Route path="/workoutlist" element={<WorkoutList />} />
+        <Route path="/login" element={<Login />} />
       </Routes>
-    
-    
- </Router>
-
+    </Router>
   );
 }
-
-
-
 
 export default App;
