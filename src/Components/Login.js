cca3d1ef8f01785b6586a4624b40cec94ee5c8d3
Sakib Ahmed <sakib.ahmed@onepeloton.com>
2022-02-13 17:08:34 -0500

    Fix auth

--- src/Components/Login.js
+++ src/Components/Login.js
@@ -1,17 +1,32 @@
 import React from "react";
-import { GoogleAuthProvider } from "firebase/auth";
-import { getAuth, signInWithRedirect } from "firebase/auth";
-
-
+import { getAuth, signInWithPopup, GoogleAuthProvider } from "firebase/auth";
 
 const Login = () => {
-const loginWithGoogle =()=>{
-const provider = new GoogleAuthProvider();
-const auth = getAuth();
-signInWithRedirect(auth, provider);
-}
+  const loginWithGoogle = () => {
+    const provider = new GoogleAuthProvider();
+    const auth = getAuth();
+    signInWithPopup(auth, provider)
+      .then((result) => {
+        // This gives you a Google Access Token. You can use it to access the Google API.
+        const credential = GoogleAuthProvider.credentialFromResult(result);
+        const token = credential.accessToken;
+        // The signed-in user info.
+        const user = result.user;
+        console.log(user);
+        // ...
+      })
+      .catch((error) => {
+        // Handle Errors here.
+        const errorCode = error.code;
+        const errorMessage = error.message;
+        // The email of the user's account used.
+        const email = error.email;
+        // The AuthCredential type that was used.
+        const credential = GoogleAuthProvider.credentialFromError(error);
+        // ...
+      });
+  };
   return (
-    
     <div className="navbar">
       {/* <form>
         <div className="input-container">
@@ -26,7 +41,7 @@ signInWithRedirect(auth, provider);
           <input type="submit" />
         </div>
       </form> */}
-      <button onClick = {loginWithGoogle}> Login Google</button>
+      <button onClick={loginWithGoogle}> Login Google</button>
     </div>
   );
 };
