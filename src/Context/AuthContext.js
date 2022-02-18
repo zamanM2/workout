9c21744d492213529f2656a209f917ed6c7c30f3
Sakib Ahmed <sakib.ahmed@onepeloton.com>
2022-02-17 21:33:21 -0500

    auth stuff

--- src/Context/AuthContext.js
+++ src/Context/AuthContext.js
@@ -1,11 +1,18 @@
-import React, { createContext, useState } from "react";
+import React, { createContext, useContext, useState } from "react";
+import { auth } from "../Firebase/FirebaseConfig";
 
 const AuthContext = createContext({});
+
+export function useAuth() {
+  return useContext(AuthContext);
+}
 
 const AuthProvider = ({ children }) => {
   const [currentUser, setCurrentUser] = useState(null);
 
   const contextValue = { currentUser };
+
+  const login = () => {};
 
   return (
     <AuthProvider.Provider value={contextValue}>
