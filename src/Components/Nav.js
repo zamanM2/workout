74e220c08e0c73f99968ef54309613aea4e7c653
Sakib Ahmed <sakib.ahmed@onepeloton.com>
2022-02-19 22:04:26 -0500

    signout

--- src/Components/Nav.js
+++ src/Components/Nav.js
@@ -1,14 +1,21 @@
 import React from "react";
 import { Link } from "react-router-dom";
+import { getAuth, signOut } from "firebase/auth";
 
 const Nav = () => {
+  const handleSignOut = () => {
+    const auth = getAuth();
+    signOut(auth)
+      .then(() => {})
+      .catch((error) => {});
+  };
   return (
     <nav className="navbar">
       <Link className="navbarLink" to="/workoutlist">
         WorkoutList
       </Link>
-      <Link className="navbarLink" to="/">
-        Login
+      <Link onClick={handleSignOut} className="navbarLink" to="/">
+        Signout
       </Link>
     </nav>
   );
