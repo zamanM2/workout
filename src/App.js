caa71379138454cfc444dfdb671ee1217c8ad9b2
Sakib Ahmed <sakib.ahmed@onepeloton.com>
2022-02-19 20:54:31 -0500

    redirectt user after login

--- src/App.js
+++ src/App.js
@@ -2,7 +2,7 @@ import "./App.css";
 import React from "react";
 import WorkoutList from "./Components/WorkoutList";
 import { Login } from "./Components/Login";
-import { BrowserRouter as Router, Routes, Route, Link } from "react-router-dom";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import AuthProvider from "./Context/AuthContext";
 import PrivateRoute from "./Components/PrivateRoute";
 import Nav from "./Components/Nav";
@@ -11,13 +11,14 @@ function App() {
   return (
     <Router>
       <AuthProvider>
-        <Nav />
         <Routes>
           <Route path="/" exact element={<Login />} />
+          <Route path="/login" element={<Login />} />
           <Route
             path="/workoutlist"
             element={
               <PrivateRoute>
+                <Nav />
                 <WorkoutList />
               </PrivateRoute>
             }
--- src/Components/Login.js
+++ src/Components/Login.js
@@ -1,11 +1,14 @@
 import React from "react";
 import { useAuth } from "../Context/AuthContext";
+import { useNavigate } from "react-router-dom";
 
 const Login = () => {
   const { login } = useAuth();
+  const navigate = useNavigate();
 
   const handleLogin = () => {
     login();
+    navigate("/workoutlist");
   };
 
   return (
