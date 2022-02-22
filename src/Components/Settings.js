13068eabe10fe5fed441e0d019ffb0891ed9e2a9
Sakib Ahmed <sakib.ahmed@onepeloton.com>
2022-02-21 19:48:51 -0500

    timer

--- src/Components/Settings.js
+++ src/Components/Settings.js
@@ -11,13 +11,13 @@ const Settings = () => {
   return (
     <Container>
       <Container className="d-flex">
-        <h3 className="">Timer Length: {timer}s</h3>
+        <h4 className="">Rest Timer: {timer}s</h4>
         <Button style={{ marginLeft: "5px" }} className="">
           Change
         </Button>
       </Container>
       <Container>
-        <h3>Email: {currentUser.email}</h3>
+        <h4>Email: {currentUser.email}</h4>
       </Container>
     </Container>
   );
