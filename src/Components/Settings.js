d62a6a0fa6f3fce405f1ee37b91052dda9c563c6
Sakib Ahmed <sakib.ahmed@onepeloton.com>
2022-02-21 19:44:28 -0500

    fix settings

--- src/Components/Settings.js
+++ src/Components/Settings.js
@@ -1,10 +1,26 @@
 import React from "react";
 import Container from "react-bootstrap/Container";
 import { useTimer } from "../Context/TimerContext";
+import Button from "react-bootstrap/Button";
+import { useAuth } from "../Context/AuthContext";
 
 const Settings = () => {
+  const { currentUser } = useAuth();
   const { timer } = useTimer();
-  return <Container>{timer}</Container>;
+
+  return (
+    <Container>
+      <Container className="d-flex">
+        <h3 className="">Timer Length: {timer}s</h3>
+        <Button style={{ marginLeft: "5px" }} className="">
+          Change
+        </Button>
+      </Container>
+      <Container>
+        <h3>Email: {currentUser.email}</h3>
+      </Container>
+    </Container>
+  );
 };
 
 export default Settings;
