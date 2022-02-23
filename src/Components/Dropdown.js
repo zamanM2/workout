24513376e6dc2f9d92c5267029a859e590848f2a
Sakib Ahmed <sakib.ahmed@onepeloton.com>
2022-02-22 22:15:36 -0500

    syntax fix

--- src/Components/Dropdown.js
+++ src/Components/Dropdown.js
@@ -1,30 +1,31 @@
-import React, {useState} from "react";
-import {MenuItems} from './MenuItems'
-import {Link} from 'react-router-dom';
+import React, { useState } from "react";
+import { MenuItems } from "./MenuItems";
+import { Link } from "react-router-dom";
 
-
-const Dropdown=()=>{
-    const [click, setClick]= useState(false)
-    const handleClick=()=> setClick(!click)
-    return(
-        <div>
-            <ul onClick= {handleClick}
-            classNmae= {click ? 'dropdown-menu clicked' : 'dropdown-menu'}
+const Dropdown = () => {
+  const [click, setClick] = useState(false);
+  const handleClick = () => setClick(!click);
+  return (
+    <div>
+      <ul
+        onClick={handleClick}
+        className={click ? "dropdown-menu clicked" : "dropdown-menu"}
+      />
+      {MenuItems.map((item, index) => {
+        return (
+          <li key={index}>
+            <Link
+              className={item.className}
+              to={item.path}
+              onClick={() => setClick(false)}
             >
-                
-            </ul>
-            {MenuItems.map((item,index) =>{
-                return(
-                    <li key={index}>
-                        <Link className={item.className} to={item.path} onClick={()=>
-                        setClick(false)}>
-                            {item.title}
-                        </Link>
-                    </li>
-                )
-            })}
-        </div>
-    )
-}
+              {item.title}
+            </Link>
+          </li>
+        );
+      })}
+    </div>
+  );
+};
 
 export default Dropdown;
