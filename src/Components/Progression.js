653a475b347bff482a0acc1ef5080a845e220dc0
Sakib Ahmed <sakib.ahmed@onepeloton.com>
2022-02-27 20:34:54 -0500

    css

--- src/Components/Progression.js
+++ src/Components/Progression.js
@@ -14,6 +14,7 @@ import { useParams } from "react-router-dom";
 import Container from "react-bootstrap/Container";
 import { useAuth } from "../Context/AuthContext";
 import { getLogHistory } from "../Firebase/WorkoutApi";
+import "../css/blackBtn.css";
 
 ChartJS.register(
   CategoryScale,
@@ -69,7 +70,9 @@ const Progression = (props) => {
     <Container>
       <Line options={options} data={data} type={"line"} />
       <br />
-      <button onClick={() => {}}>Back</button>
+      <button onClick={() => {}} className="blackBtn">
+        Back
+      </button>
     </Container>
   );
 };
