7c29ee8077a0a597052afb84d441148ddb4dad70
Sakib Ahmed <sakib.ahmed@onepeloton.com>
2022-03-01 01:30:06 -0500

    fix

--- src/Components/Progression.js
+++ src/Components/Progression.js
@@ -57,34 +57,30 @@ const Progression = (props) => {
   const navigate = useNavigate();
   let { id } = useParams();
 
-
   useEffect(() => {
     async function fetchLogData() {
       await getLogHistory(currentUser.uid, id).then((snapshot) => {
-        const keys = Object.keys(snapshot.val());
-        data.labels = keys;
-        const myDataArray = [
+        const dates = Object.keys(snapshot.val());
+        data.labels = dates; //x-axis
+        const weightData = [
           {
-            label: "Yellow",
+            label: "Exercise",
             data: [],
             borderColor: "rgb(255, 99, 132)",
             backgroundColor: "rgba(255, 99, 132, 0.5)",
           },
         ];
-        for (let i = 0; i < keys.length; i++) {
-          let max =-1;
-          for (let j = 0; j < snapshot.val()[keys[i]].length; j++) {
-          if(snapshot.val()[keys[i]][j].weight > max){
-            max = snapshot.val()[keys[i]][j].weight;
+        for (let i = 0; i < dates.length; i++) {
+          let max = -1;
+          for (let j = 0; j < snapshot.val()[dates[i]].length; j++) {
+            if (snapshot.val()[dates[i]][j].weight > max) {
+              max = snapshot.val()[dates[i]][j].weight;
+            }
           }
-          }
-           myDataArray[0].data.push(max)
+          weightData[0].data.push(max);
         }
-
-        data.datasets = myDataArray;
-
+        data.datasets = weightData;
         updateState({});
-
       });
     }
     fetchLogData();
